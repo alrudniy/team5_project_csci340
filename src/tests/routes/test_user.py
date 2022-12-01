9254e861fe8872dde648a24aad4b8d3fadb04a26
PixelMasterNate <nrene@drew.edu>
2022-12-01 12:35:59 -0500

    Added delete user endpoint without auth

--- src/tests/routes/test_user.py
+++ src/tests/routes/test_user.py
@@ -6,3 +6,10 @@ def test_get_user_auth_required(client: TestClient):
     assert response.status_code == 403  # Forbidden
     response_data = response.json()
     assert "detail" in response_data
+
+# Test the Delete user endpoint without auth - DELETE /api/v1/user
+def test_delete_user_auth_required(client: TestClient):
+    response = client.delete("/api/v1/user")
+    assert response.status_code == 403
+    response_data = response.json()
+    assert "detail" in response_data
