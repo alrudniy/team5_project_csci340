e1926bbc69b79da9c78e92a1228a9919d27d1571
Jaewoo Park <jpark9@drew.edu>
2022-12-01 12:30:09 -0500

    test_delete added to test_bucket.py

--- src/tests/routes/test_bucket.py
+++ src/tests/routes/test_bucket.py
@@ -6,3 +6,10 @@ def test_create_bucket_auth_required(client: TestClient):
     assert response.status_code == 403  # Forbidden
     response_data = response.json()
     assert "detail" in response_data
+
+# Test the Bucket delete endpoint without auth - DELETE /api/v1/bucket/{bucket_id}
+def test_delete_bucket_auth_required(client: TestClient):
+    response = client.delete("/api/v1/bucket/1")
+    assert response.status_code == 403  # Forbidden
+    response_data = response.json()
+    assert "detail" in response_data
