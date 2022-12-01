bdf0fa1d5c3accfc3b860e98207fd49392475b6d
Henry <harthur@drew.edu>
2022-12-01 12:11:52 -0500

    initial commit

--- src/tests/routes/test_bucket.py
+++ src/tests/routes/test_bucket.py
@@ -6,3 +6,7 @@ def test_create_bucket_auth_required(client: TestClient):
     assert response.status_code == 403  # Forbidden
     response_data = response.json()
     assert "detail" in response_data
+
+# Test the Bucket update endpoint without auth - PUT /api/v1/bucket/{bucket_id}
+
+def test_update_bucket_auth_required(client: TestClient):
