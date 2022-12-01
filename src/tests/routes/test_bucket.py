ce580e54a35c3a2d01ba258be4a06485211a9deb
Henry <harthur@drew.edu>
2022-12-01 12:26:00 -0500

    Henry

--- src/tests/routes/test_bucket.py
+++ src/tests/routes/test_bucket.py
@@ -7,6 +7,9 @@ def test_create_bucket_auth_required(client: TestClient):
     response_data = response.json()
     assert "detail" in response_data
 
-# Test the Bucket update endpoint without auth - PUT /api/v1/bucket/{bucket_id}
-
-def test_update_bucket_auth_required(client: TestClient):
+# Test the Bucket creation endpoint without auth - POST /api/v1/bucket
+def test_create_bucket_auth_required(client: TestClient):
+    response = client.post("/api/v1/bucket")
+    assert response.status_code == 403  # Forbidden
+    response_data = response.json()
+    assert "detail" in response_data
